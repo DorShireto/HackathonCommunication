c6e3a5f605fbc8a8da5d8964381b11d4439460d1
Dor Shireto <shireto.dor@gmail.com>
2020-12-29 19:49:57 +0200

    Hey

--- Client.py
+++ Client.py
@@ -1,19 +1,34 @@
 import getch
-from socket import * 
+import socket
+import struct
 
-
-
-serverName ="127.0.0.1"
 serverPort = 2058 
-clientSocket = socket(AF_INET,SOCK_STREAM)
-#TODO: find server to connect to
-clientSocket.connect((serverName,serverPort))
+clientSocketTCP = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+clientSocketUDP = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+clientSocketUDP.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1) #SO_REUSEPORT allow multi client connection
+# # client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+clientSocketUDP.bind(('',13117))
+
+
+
+print("Client started, listening for offer requests from remote server...")
+
+#listening for udp over port 13117 - 1024 is buffer size
+msg, serverDetails = clientSocketUDP.recvfrom(1024)
+msg = struct.unpack('Ibh',b'10')
+print(msg)
+serverAddress = serverDetails[0]
+
+
+#serverPort = port will be in l5-6 bytes of the msg
+
+clientSocketTCP.connect((serverAddress,serverPort))
 print("connected to server...")
 #TODO:handle connection exception
 while True:
     char = getch.getch()
     print(char) #TODO debug
-    clientSocket.send(char.encode())
+    clientSocketTCP.send(char.encode())
 
 
     
--- Server.py
+++ Server.py
@@ -1,29 +1,54 @@
 from socket import *
+import struct
+import threading
+import time
+#### ATR ###
 serverPort = 2058
+connections=[]
+addresses=[]
 
-#UDP
-#serverSocketUDP = socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP)
-#serverSocketUDP.setsockopt(SOL_SOCKET, SO_BROADCAST, 1) # Broadcast connection over UDP
-
-
+def acceptClients():
+    while True:
+        connectionSocketTCP, addr = serverSocketDoor.accept()
+        connections.append(connectionSocketTCP)
+        addresses.append(addr)
+        print("established connection with",addr)
 
 
 # TCP
-serverSocketTCP = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP)
-serverSocketTCP.bind(("",serverPort))
-serverSocketTCP.listen() # 1 - mean how many clients can connect
-connectionSocketTCP, addr = serverSocketTCP.accept()
+serverSocketDoor = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP)
+serverSocketDoor.bind(("",serverPort))
+serverSocketDoor.listen() # 1 - mean how many clients can connect
+acceptThread = threading.Thread(target=acceptClients)
+acceptThread.daemon=True
+acceptThread.start()
 
+
+#UDP
+# serverSocketUDP = socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP)
+serverSocketUDP = socket(AF_INET,SOCK_DGRAM)
+serverSocketUDP.setsockopt(SOL_SOCKET, SO_REUSEADDR ,1)
+serverSocketUDP.setsockopt(SOL_SOCKET, SO_BROADCAST, 1) # Broadcast connection over UDP
+serverSocketUDP.bind(("",serverPort))
+#serverSocketUDP.settimeout(10)#close the socket after 10 sec
+print("Server started, listening on IP address 132.72.200.153")
+msg = struct.pack('Ibh',4276993775,2,2058)
+# msg = b"YOUVE BEEN HACKED BY US. YOUR CODE WILL BE DELETED IN 3 Minutes"
+while 1:
+    # serverSocketUDP.sendto(msg,('255.255.255.255',13117)) #turn on when client is other host
+    serverSocketUDP.sendto(msg,('255.255.255.255',13117)) # use this line only in debug mode
+    print("UDP broadcast inventation sent\n")
+    # time.sleep(1)
 
 
 
 
 while True:
-    char = connectionSocketTCP.recv(1024)
-    if char == "":
-        connectionSocketTCP.close()
-    print(char.decode())
-
+    for connectionToClient in connections:
+        char = connectionToClient.recv(1024)
+        if char == "":
+            connectionToClient.close()
+        print(char.decode())
 
 
 

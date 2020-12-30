1f111c10491f53da4ed9eef4852dd273a4eb78bc
Dor Shireto <shireto.dor@gmail.com>
2020-12-30 16:32:39 +0200

    4

--- Client.py
+++ Client.py
@@ -1,20 +1,20 @@
-# import getch
-import msvcrt
+import getch
+# import msvcrt
 import socket
 import struct
 
 def getchar():
-    return msvcrt.getche()
-    #return getch.getch() #uncomment me while using LINUX
+    # return msvcrt.getche()
+    return getch.getch() #uncomment me while using LINUX
 
 
-
+teamName = "DeaD_l0ck_Av0idErs"
 gotPort=False
 clientSocketTCP = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 clientSocketUDP = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-# clientSocketUDP.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1) #SO_REUSEPORT allow multi client connection
+clientSocketUDP.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1) #SO_REUSEPORT allow multi client connection
 # # client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-clientSocketUDP.bind(('',13117))
+clientSocketUDP.bind(('',13115))
 
 
 
@@ -32,9 +32,12 @@ serverAddress = serverDetails[0]
 
 
 #serverPort = port will be in l5-6 bytes of the msg
-
+print("Received offer from " + serverAddress +" attempting to connect... ”")
 clientSocketTCP.connect((serverAddress,serverPort))
 print("connected to server via TCP...")
+#sending client name to server over TCP (5)
+clientSocketTCP.send(teamName.encode())
+
 
 msg = clientSocketTCP.recv(1024)
 print(msg.decode())
@@ -45,7 +48,7 @@ print(msg.decode())
 while True:
     char = getchar()
     print(char) #TODO debug
-    clientSocketTCP.send(char)
+    clientSocketTCP.send(char.encode())
 
 
     
--- Server.py
+++ Server.py
@@ -2,21 +2,17 @@ from socket import *
 import struct
 import threading
 import time
-#### ATRIBUTES ###
 
-def acceptClients():
-    while True:
-        connectionSocketTCP, addr = serverSocket.accept()
-        connectionsList.append(connectionSocketTCP)
-        addresses.append(addr)
-        print("established connection with",addr)
-        connectionSocketTCP.send(instructionsMsg.encode())
-
+#**********************************************************************************************
+#                                          ATRIBUTES
+#
+#**********************************************************************************************
 
 serverPort = 2058
 serverName = "DeaD_l0ck_Av0idErs"
 connectionsList = []
 addresses=[]
+players = {} # key: client address | value: [clientName, clientTeam(A or B)]
 instructionsMsg = "************************************************\n " \
                   "Hello Player!!!\n" \
                   "Welcome to "+serverName+" server.\n" \
@@ -25,51 +21,106 @@ instructionsMsg = "************************************************\n " \
                   "Now wait for starting announcement\n" \
                   "************************************************"
 
-# TCP
-serverSocket = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP)
-serverSocket.bind(("",serverPort))
-serverSocket.listen() # 1 - mean how many clients can connect
-acceptThread = threading.Thread(target=acceptClients)
-acceptThread.daemon=True
-acceptThread.start()
+
+#**********************************************************************************************
+#                                       FUNCTIONS PART
+#
+#**********************************************************************************************
+def acceptClients():
+    while True:
+        connectionSocketTCP, addr = serverSocket.accept()
+        connectionsList.append(connectionSocketTCP)
+        addresses.append(addr)
+        clientName = connectionSocketTCP.recv(1024)
+        print(clientName.decode())
+        print("established connection with",addr)
+        connectionSocketTCP.send(instructionsMsg.encode())
 
 
-#UDP
-# serverSocketUDP = socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP)
-serverSocketUDP = socket(AF_INET,SOCK_DGRAM)
-serverSocketUDP.setsockopt(SOL_SOCKET, SO_REUSEADDR ,1)
-serverSocketUDP.setsockopt(SOL_SOCKET, SO_BROADCAST, 1) # Broadcast connection over UDP
-serverSocketUDP.bind(("",serverPort))
-#serverSocketUDP.settimeout(10)#close the socket after 10 sec
-print("Server started, listening on IP address 132.72.200.153")
-# var = struct.pack('bi', 56, 0x12131415)
-msg = struct.pack('Ibh',4276993775,2,2058)
-cnt=0
-while cnt<10:
-    # serverSocketUDP.sendto(msg,('255.255.255.255',13117)) #turn on when client is other host
-    serverSocketUDP.sendto(msg,('255.255.255.255',13117)) # use this line only in debug mode
-    print("UDP broadcast inventation sent\n")
-    time.sleep(1)
-    cnt+=1
+def createUDPSocket():
+    # UDP
+    # serverSocketUDP = socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP)
+    serverSocketUDP = socket(AF_INET, SOCK_DGRAM)
+    serverSocketUDP.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    serverSocketUDP.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)  # Broadcast connection over UDP
+    serverSocketUDP.bind(("", serverPort))
+
+def createTCPSocket():
+    serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)
+    serverSocket.bind(("", serverPort))
+    serverSocket.listen()  # 1 - mean how many clients can connect
+    acceptThread = threading.Thread(target=acceptClients)
+    acceptThread.daemon = True
+    acceptThread.start()
+
+def broadcastUDP():
+    # var = struct.pack('bi', 56, 0x12131415)
+    msg = struct.pack('Ibh', 4276993775, 2, 2058)
+    timeout = 10
+    startTime = time.time()
+    while timeout > time.time() - startTime :
+        # serverSocketUDP.sendto(msg,('255.255.255.255',13117)) #turn on when client is other host
+        serverSocketUDP.sendto(msg, ('255.255.255.255', 13115))  # use this line only in debug mode
+        print("UDP broadcast inventation sent\n") #TODO debug
 
 
-for TCP_connectionToClient in connectionsList:
+def acceptClients():
+    while True:
+        team = 1
+        connectionSocketTCP, addr = serverSocket.accept()
+        connectionsList.append(connectionSocketTCP)
+        addresses.append(addr)
+        clientName = connectionSocketTCP.recv(1024)
+        players[addr] = [clientName,team]
+        if team == 2:
+            team = 1
+        else:
+            team = 2
+        print(clientName.decode())
+        print("established connection with",addr)
+        connectionSocketTCP.send(instructionsMsg.encode())
 
-    TCP_connectionToClient.send(b'GAME ON !!!!!!!!\nSTART SPAMMING !!!!!!!')
+def closePreGameSockets():
+    serverSocket.close()
+    serverSocketUDP.close()
+
+
+def gameOnAnnouncment():
+    for TCP_connectionToClient in connectionsList:
+        TCP_connectionToClient.send(b'GAME ON !!!!!!!!\nSTART SPAMMING !!!!!!!')
+
+def summaryGame():
+
+
+def game():
+    while True:
+        for connectionToClient in connectionsList:
+            connectionToClient.settimeout(0.1)
+            try:
+                char = connectionToClient.recv(1024)
+                
+                # print("ignore " ,ignore)
+                # if char == "":
+                #     connectionToClient.close()
+                print(char.decode())
+            except Exception as e:
+                continue
+
+#**********************************************************************************************
+#                         SCRIPT PART
+#
+#**********************************************************************************************
+
+
+while 1:
+    createTCPSocket()
+    createUDPSocket()
+    broadcastUDP()
+    closePreGameSockets()
+    gameOnAnnouncment()
+    game()
+    summaryGame()
 
 
 
-
-while True:
-    for connectionToClient in connectionsList:
-        ignore = connectionToClient.recv(1024)
-        print("ignore " ,ignore)
-        if char == "":
-            connectionToClient.close()
-        print(char.decode())
-
-
-
-
-#def sendBroadcastInvite():
     

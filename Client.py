b5ec43ac55829e47bc91475b479343d23ef2e9ca
Dor Shireto <shireto.dor@gmail.com>
2020-12-30 23:40:37 +0200

    Ver 6

--- Client.py
+++ Client.py
@@ -1,56 +1,85 @@
+
 import getch
 # import msvcrt
 import socket
 import struct
 import time
+from scapy.all import *
+
+class Client:
 #**********************************************************************************************
 #                                          ATRIBUTES
 #
 #**********************************************************************************************
-teamName = "DeaD_l0ck_Av0idErs"
-gotPort=False
-serverAddress = None
-clientSocketTCP = None
+    def __init__(self):
+        self.teamName = "DeaD_l0ck_Av0idErs"
+        self.serverPort = None
+        self.gotPort=False
+        self.serverAddress = None
+        self.clientSocketTCP = None
+        self.clientSocketUDP = None
+        self.serverAddress= None
+        self.setupConnections()
 
-#**********************************************************************************************
-#                                       FUNCTIONS PART
-#
-#**********************************************************************************************
+    #**********************************************************************************************
+    #                                       FUNCTIONS PART
+    #
+    #**********************************************************************************************
 
-def getchar():
-    # return msvcrt.getche()
-    return getch.getch() #uncomment me while using LINUX
-
-
-def setupConnections():
-    clientSocketTCP = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    clientSocketUDP = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-    clientSocketUDP.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1) #SO_REUSEPORT allow multi client connection
-    # # client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    clientSocketUDP.bind(('',13115)) #TODO change port before submitting 13117!!!!!!
+    def getchar(self):
+        # return msvcrt.getche()
+        return getch.getch() #uncomment me while using LINUX
 
 
+    def setupConnections(self):
+
+        self.clientSocketTCP = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.clientSocketUDP = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        self.clientSocketUDP.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1) #SO_REUSEPORT allow multi client connection
+        # # client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.clientSocketUDP.bind(('',13115)) #TODO change port before submitting 13117!!!!!!
+        self.serverAddress = get_if_addr("eth1")
 
 
-def searchForServer():
-    #listening for udp over port 13117 - 1024 is buffer size
-    while not gotPort:
-        msg, serverDetails = clientSocketUDP.recvfrom(1024)
-        msg = struct.unpack('Ibh',msg)
-        if msg[0] == 4276993775 and msg[1] == 2:
-            serverPort=msg[2]
-            gotPort=True
-    serverAddress = serverDetails[0]
-    print("Received offer from " + serverAddress +" attempting to connect... ”")
 
-def game():
-    timeout = 10
-    startTime = time.time()
-    while timeout > time.time() - startTime :        
-        char = getchar()
-        print(char) #TODO debug
-        clientSocketTCP.send(char.encode())
 
+    def searchForServer(self):
+        #listening for udp over port 13117 - 1024 is buffer size
+        while not self.gotPort:
+            msg, serverDetails = self.clientSocketUDP.recvfrom(1024)
+            msg = struct.unpack('Ibh',msg)
+            if msg[0] == 4276993775 and msg[1] == 2:
+                self.serverPort=msg[2]
+                self.gotPort=True
+        serverAddress = serverDetails[0]
+        print("Received offer from " + serverAddress +" attempting to connect... ”")
+
+    def game(self):
+        os.system("stty raw -echo")
+        while 1:
+            msgFromServerWaiting, _, _ = select([self.clientSocketTCP],[],[],0.2)
+            if msgFromServerWaiting:
+                os.system("stty -raw echo")
+                return
+            else: # no waiting msg from server -> GAME MODE STILL RUNING
+                thereIsTypedChar, _, _ = select([sys.stdin],[],[],0.2)
+                if thereIsTypedChar:
+                    #get the char from IO
+                    char = sys.stdin.read(1)
+                    self.clientSocketTCP.send(char.encode())
+
+
+
+
+
+
+        # timeout = 10
+        # startTime = time.time()
+        # while timeout > time.time() - startTime :
+        #     char = self.getchar()
+        #     print(char) #TODO debug
+        #     self.clientSocketTCP.send(char.encode())
+        
 
 #**********************************************************************************************
 #                         SCRIPT PART
@@ -58,17 +87,20 @@ def game():
 #**********************************************************************************************
 print("Client started, listening for UDP offer requests from remote server...")
 while 1:
-    setupConnections()
-    searchForServer()
+    client = Client()
+    # client.setupConnections()
+    client.searchForServer()
     #connect to server
-    clientSocketTCP.connect((serverAddress,serverPort))
+    print(client.serverAddress)
+    print(client.serverPort)
+    client.clientSocketTCP.connect((client.serverAddress,client.serverPort))
     print("connected to server via TCP...") #TODO DEBUG
     #sending client name to server over TCP (5)
-    clientSocketTCP.send(teamName.encode())
-    msg = clientSocketTCP.recv(1024) #welcomeMSG
+    client.clientSocketTCP.send(client.teamName.encode())
+    msg = client.clientSocketTCP.recv(1024) #welcomeMSG
     print(msg.decode())
-    game()
-    msg = clientSocketTCP.recv(1024) #summaryMSG
+    client.game()
+    msg = client.clientSocketTCP.recv(1024) #summaryMSG
     print(msg.decode())
     print("Server disconnected, listening for offer requests...")
 
--- Server.py
+++ Server.py
@@ -3,183 +3,200 @@ import struct
 import threading
 import time
 
-#**********************************************************************************************
-#                                          ATRIBUTES
-#
-#**********************************************************************************************
 
-serverPort = 2058
-serverName = "DeaD_l0ck_Av0idErs"
-connectionsList = []
-addresses=[]
-teamANames = []
-teamBNames = []
-scoreA = 0
-scoreB = 0
-players = {} # key: client address | value: [clientName, clientTeam(A or B)]
-# instructionsMsg = "************************************************\n " \
-#                   "Hello Player!!!\n" \
-#                   "Welcome to "+serverName+" server.\n" \
-#                   " game will start in 10 seconds.\n" \
-#                   " Your goal is to type as much charecters as you can within 10 seconds\n" \
-#                   "Now wait for starting announcement\n" \
-#                   "************************************************"
-
-
-#**********************************************************************************************
-#                                       FUNCTIONS PART
-#
-#**********************************************************************************************
-def acceptClients():
-    while True:
-        connectionSocketTCP, addr = serverSocket.accept()
-        connectionsList.append(connectionSocketTCP)
-        addresses.append(addr)
-        clientName = connectionSocketTCP.recv(1024)
-        print(clientName.decode())
-        print("established connection with",addr)
-        # connectionSocketTCP.send(instructionsMsg.encode())
-
-
-def createUDPSocket():
-    # UDP
-    # serverSocketUDP = socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP)
-    serverSocketUDP = socket(AF_INET, SOCK_DGRAM)
-    serverSocketUDP.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    serverSocketUDP.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)  # Broadcast connection over UDP
-    serverSocketUDP.bind(("", serverPort))
-
-def createTCPSocket():
-    serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)
-    serverSocket.bind(("", serverPort))
-    serverSocket.listen()  # 1 - mean how many clients can connect
-    acceptThread = threading.Thread(target=acceptClients)
-    acceptThread.daemon = True
-    acceptThread.start()
-
-def broadcastUDP():
-    # var = struct.pack('bi', 56, 0x12131415)
-    msg = struct.pack('Ibh', 4276993775, 2, 2058)
-    timeout = 10
-    startTime = time.time()
-    while timeout > time.time() - startTime :
-        # serverSocketUDP.sendto(msg,('255.255.255.255',13117)) #turn on when client is other host
-        serverSocketUDP.sendto(msg, ('255.255.255.255', 13115))  # use this line only in debug mode
-        print("UDP broadcast inventation sent\n") #TODO debug
-
-
-def acceptClients():
-    while True:
-        team = 1
-        connectionSocketTCP, addr = serverSocket.accept()
-        connectionsList.append(connectionSocketTCP)
-        addresses.append(addr)
-        clientName = connectionSocketTCP.recv(1024)
-        players[addr] = [clientName,team]
-        if team == 2:
-            teamBNames.append(clientName)
-            team = 1
-        else:
-            teamANames.append(clientName)
-            team = 2
-        print(clientName.decode())
-        print("established connection with",addr) #TODO debug
-        connectionSocketTCP.send(instructionsMsg.encode())
-
-def closePreGameSockets():
-    # acceptThread.join()
-    serverSocket.close()
-    serverSocketUDP.close()
-
-
-def gameOnAnnouncment():
-    welcomeMSG = "Welcome to Keyboard Spamming Battle Royale.\nGroup A:\n==\n"
-    for teamName in teamANames:
-        welcomeMSG+=teamName
-    welcomeMSG+="Group B:\n==\n"
-    for teamName in teamBNames:
-        welcomeMSG+=teamName
-    welcomeMSG += "Start pressing keys on your keyboard as fast as you can!!"
-    for TCP_connectionToClient in connectionsList:
-        TCP_connectionToClient.send(welcomeMSG.encode())
-
-def summaryGame():
-    summaryMSG = "Game over!\n Group A typed in " + scoreA + " characters.\n Group B typed in " + scoreB + " characters.\n"
-    winningTeam = ""
-    if scoreA > scoreB:
-        winningTeam = "A"
-        summaryMSG += "Group "+ winningTeam + " wins!\n\nCongratulations to the winners:\n==\n"
-        #loop over players and get clients names
-        for name in teamANames:
-            summaryMSG+=name
-
-    elif scoreB > scoreA:
-        winningTeam = "B"
-        summaryMSG += "Group "+ winningTeam + " wins!\n\nCongratulations to the winners:\n==\n"
-        #loop over players and get clients names
-        for name in teamBNames:
-            summaryMSG+=name
-    else:
-        summaryMSG += "It's a draw!!\n"
-
-    #iterate over all conections and send summaryMSG
-    for TCP_connectionToClient in connectionsList:
-        TCP_connectionToClient.send(summaryMSG.encode())
-
-
-
-
-
-# players = {} # key: client address | value: [clientName, clientTeam(A or B)]
-
-def game():
-    timeout = 10
-    startTime = time.time()
-    while timeout > time.time() - startTime :
-        for connectionToClient in connectionsList:
-            connectionToClient.settimeout(0.1)
-            try:
-                char, clientAddr = connectionToClient.recvfrom(1)
-                if players[clientAddr][1] == 1: #team A
-                    scoreA += 1
-                else:
-                    scoreB += 1
-                # print("ignore " ,ignore)
-                # if char == "":
-                #     connectionToClient.close()
-                print(char.decode())
-            except Exception as e:
-                continue
-
-def clearServerInfo():
-    #close all TCP sockets
-    for TCP_connectionToClient in connectionsList:
-        TCP_connectionToClient.close()
-    connectionsList = []
-    addresses=[]
-    teamANames = []
-    teamBNames = []
-    scoreA = 0
-    scoreB = 0
-    players = {}
-
-
-#**********************************************************************************************
-#                         SCRIPT PART
-#
-#**********************************************************************************************
-
-
-while 1:
-    createTCPSocket()
-    createUDPSocket()
-    broadcastUDP()
-    closePreGameSockets()
-    gameOnAnnouncment()
-    game()
-    summaryGame()
-    print("Game over, sending out offer requests...")
-
-
+class Server:
+    # **********************************************************************************************
+    #                                          ATRIBUTES
+    #
+    # **********************************************************************************************
+    def __init__(self):
+        self.serverSocketUDP = None
+        self.serverSocket = None
+        self.serverPort = 2058
+        self.serverName = "DeaD_l0ck_Av0iders"
+        self.connectionsList = []
+        self.addresses = []
+        self.teamANames = []
+        self.teamBNames = []
+        self.scoreA = 0
+        self.scoreB = 0
+        self.players = {}  # key: client address | value: [clientName, clientTeam(A or B)]
 
     
+
+    # **********************************************************************************************
+    #                                       FUNCTIONS PART
+    #
+    # **********************************************************************************************
+    
+    def createUDPSocket(self):
+        # UDP
+        # serverSocketUDP = socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP)
+        self.serverSocketUDP = socket(AF_INET, SOCK_DGRAM)
+        self.serverSocketUDP.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self.serverSocketUDP.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)  # Broadcast connection over UDP
+        self.serverSocketUDP.bind(("", self.serverPort))
+
+    def createTCPSocket(self):
+        if self.serverSocket is not None:
+            try:   
+                self.serverSocket.close()
+            except:
+                self.serverSocket = None
+        self.serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)
+        self.serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self.serverSocket.bind(("", self.serverPort))
+        self.serverSocket.listen(20)  # 1 - mean how many clients can connect
+        acceptThread = threading.Thread(target=self.acceptClients)
+        acceptThread.daemon = True
+        acceptThread.start()
+
+    def broadcastUDP(self):
+        # var = struct.pack('bi', 56, 0x12131415)
+        msg = struct.pack('Ibh', 4276993775, 2, 2058)
+        timeout = 10
+        startTime = time.time()
+        while timeout > time.time() - startTime:
+            # serverSocketUDP.sendto(msg,('255.255.255.255',13117)) #turn on when client is other host
+            self.serverSocketUDP.sendto(msg, ('255.255.255.255', 13115))  # use this line only in debug mode
+            print("UDP broadcast inventation sent\n")  # TODO debug
+            time.sleep(1)
+
+    def acceptClients(self):
+        team = 1
+        while True:
+            connectionSocketTCP, addr = self.serverSocket.accept()
+            self.connectionsList.append(connectionSocketTCP)
+            self.addresses.append(addr)
+            clientName = connectionSocketTCP.recv(1024).decode()
+            self.players[connectionSocketTCP] = [clientName, team]
+            if team == 2:
+                self.teamBNames.append(clientName)
+                team = 1
+            else:
+                self.teamANames.append(clientName)
+                team = 2
+            print(clientName)
+            print("established connection with", addr)  # TODO debug
+            print("addres is : ",addr)
+            # connectionSocketTCP.send(instructionsMsg.encode())
+
+    def closePreGameSockets(self):
+        # acceptThread.join()
+        # self.serverSocket.close()
+        self.serverSocketUDP.close()
+
+    def gameOnAnnouncment(self):
+        welcomeMSG = "Welcome to Keyboard Spamming Battle Royale.\nGroup A:\n==\n"
+        for teamName in self.teamANames:
+            welcomeMSG += teamName
+        welcomeMSG += "Group B:\n==\n"
+        for teamName in self.teamBNames:
+            welcomeMSG += teamName
+        welcomeMSG += "Start pressing keys on your keyboard as fast as you can!!"
+        for TCP_connectionToClient in self.connectionsList:
+            TCP_connectionToClient.send(welcomeMSG.encode())
+
+    def summaryGame(self):
+        print(self.scoreA)
+        print(self.scoreB)
+        summaryMSG = "Game over!\nGroup A typed in " + str(self.scoreA) + " characters.\nGroup B typed in " + str(
+            self.scoreB) + " characters.\n"
+        winningTeam = ""
+        if self.scoreA > self.scoreB:
+            winningTeam = "A"
+            summaryMSG += "Group " + winningTeam + " wins!\n\nCongratulations to the winners:\n==\n"
+            # loop over players and get clients names
+            for name in self.teamANames:
+                summaryMSG += name
+
+        elif self.scoreB > self.scoreA:
+            winningTeam = "B"
+            summaryMSG += "Group " + winningTeam + " wins!\n\nCongratulations to the winners:\n==\n"
+            # loop over players and get clients names
+            for name in self.teamBNames:
+                summaryMSG += name
+        else:
+            summaryMSG += "It's a draw!!\n"
+
+        # iterate over all conections and send summaryMSG
+        for TCP_connectionToClient in self.connectionsList:
+            TCP_connectionToClient.send(summaryMSG.encode())
+
+    # players = {} # key: client address | value: [clientName, clientTeam(A or B)]
+
+    def game(self):
+        print("In game mode")
+        timeout = 10
+        startTime = time.time()
+        while timeout > time.time() - startTime:
+            for connectionToClient in self.connectionsList:
+                connectionToClient.settimeout(0.2)
+                try:
+                    char, clientAddr = connectionToClient.recvfrom(1024)                    
+                    # print("ignore " ,ignore)
+                    # if char == "":
+                    #     connectionToClient.close()
+                    print(char.decode())
+                    if self.players[connectionToClient][1] == 1:  # team A
+                        self.scoreA += 1
+                    else:
+                        self.scoreB += 1
+                except Exception as e:
+                    continue
+                
+
+    def clearServerInfo(self):
+        # close all TCP sockets
+        print("cleaning server info")
+        for TCP_connectionToClient in self.connectionsList:
+            TCP_connectionToClient.close()
+            print("tcp closed ")
+        
+        # self.serverSocketUDP = None
+        # self.serverSocket = None
+        self.connectionsList = []
+        self.addresses = []
+        self.teamANames = []
+        self.teamBNames = []
+        self.scoreA = 0
+        self.scoreB = 0
+        self.players = {}
+
+
+# **********************************************************************************************
+#                         SCRIPT PART
+#
+# **********************************************************************************************
+
+
+server = Server()
+server.createTCPSocket()
+while 1:
+    try:
+        server.createUDPSocket()
+        server.broadcastUDP()
+        server.closePreGameSockets()
+        server.gameOnAnnouncment()
+        server.game()
+        server.summaryGame()
+        print("Game over, sending out offer requests...")
+        server.clearServerInfo()
+    except Exception as e:
+        print(e)
+        continue
+
+# server = Server()
+# server.createTCPSocket()
+# server.createUDPSocket()
+# server.broadcastUDP()
+# server.closePreGameSockets()
+# server.gameOnAnnouncment()
+# server.game()
+# server.summaryGame()
+# print("Game over, sending out offer requests...")
+# server.clearServerInfo()
+
+
+
+
